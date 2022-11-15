743dad078d5c043e0a4c6b5527de80834aa5b1d3
RemoteXY <info@remotexy.com>
2022-11-15 13:15:12 +0500

    3.1.10 fix BLE overflow

--- src/RemoteXYStream_BLEDevice.h
+++ src/RemoteXYStream_BLEDevice.h
@@ -12,6 +12,9 @@
 #define REMOTEXYCOMM_BLEDEVICE__SERVICE_UUID             "0000FFE0-0000-1000-8000-00805F9B34FB" // UART service UUID
 #define REMOTEXYCOMM_BLEDEVICE__CHARACTERISTIC_UUID_RXTX "0000FFE1-0000-1000-8000-00805F9B34FB"
 
+#define REMOTEXYCOMM_BLEDEVICE__SEND_TIME_FOR_ONE_PACKAGE 7  // test min 5 ms
+#define REMOTEXYCOMM_BLEDEVICE__SEND_BYTES_BEFORE_OVERFLOW 500  // test max 600 bytes
+
 
 class CRemoteXYStream_BLEDevice : public CRemoteXYStream, BLEServerCallbacks, BLECharacteristicCallbacks {
   
@@ -25,12 +28,16 @@ class CRemoteXYStream_BLEDevice : public CRemoteXYStream, BLEServerCallbacks, BL
   uint16_t sendBufferCount;
   uint16_t sendBytesAvailable;
   
+  uint16_t sendCount;
+  uint8_t sendBLEBufferOverflow;
+  
   uint8_t receiveBuffer[REMOTEXYCOMM_BLEDEVICE__RECEIVE_BUFFER_SIZE];
   uint16_t receiveBufferStart;
   uint16_t receiveBufferPos;
   uint16_t receiveBufferCount;
   
-  volatile uint8_t receiveBufferLook;
+  volatile uint8_t receiveBufferLook; 
+    
     
   public:  
   CRemoteXYStream_BLEDevice (const char * _bleDeviceName) : CRemoteXYStream () {
@@ -87,7 +94,7 @@ class CRemoteXYStream_BLEDevice : public CRemoteXYStream, BLEServerCallbacks, BL
 #endif
     receiveBufferStart = 0;
     receiveBufferPos = 0;
-    receiveBufferCount = 0;
+    receiveBufferCount = 0; 
   };  
   
   void onDisconnect(BLEServer* pServer) {
@@ -118,20 +125,41 @@ class CRemoteXYStream_BLEDevice : public CRemoteXYStream, BLEServerCallbacks, BL
     }
   }
   
+  /*
+  // this works already after the buffer overflow and about 100 bytes are lost
+  void onStatus (BLECharacteristic *pCharacteristic, Status s, uint32_t code) {
+    if (s == Status::SUCCESS_NOTIFY) canWrite = 1; 
+  }
+  */
   
   void startWrite (uint16_t len) override {
     sendBytesAvailable = len;
     sendBufferCount = 0;
+    sendCount = 0;
+    sendBLEBufferOverflow = 0;
   }
   
   
   void write (uint8_t b) override {
+    // the ESP BLE library have some error
+    // it has some sent buffer which can overflow after 600 bytes if sending bytes without delay
+    // as a result of the tests, it was found that 5 ms per 20 bytes are needed
+    // but if send without delay, it can send up to 600 bytes
+  
     sendBuffer[sendBufferCount++] = b;
     sendBytesAvailable--;
     if ((sendBufferCount == REMOTEXYCOMM_BLEDEVICE__SEND_BUFFER_SIZE) || (sendBytesAvailable == 0)) {
       pRxTxCharacteristic->setValue((uint8_t *)sendBuffer, sendBufferCount);
       pRxTxCharacteristic->notify();
+      sendCount += sendBufferCount;
       sendBufferCount = 0;
+      if (sendCount >= REMOTEXYCOMM_BLEDEVICE__SEND_BYTES_BEFORE_OVERFLOW) {
+        if (sendBLEBufferOverflow == 0) {
+          delay (REMOTEXYCOMM_BLEDEVICE__SEND_BYTES_BEFORE_OVERFLOW / REMOTEXYCOMM_BLEDEVICE__SEND_BUFFER_SIZE * REMOTEXYCOMM_BLEDEVICE__SEND_TIME_FOR_ONE_PACKAGE);
+          sendBLEBufferOverflow = 1;
+        }
+        delay (REMOTEXYCOMM_BLEDEVICE__SEND_TIME_FOR_ONE_PACKAGE);
+      }
     }
   } 
   
